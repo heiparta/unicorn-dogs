18e3af485db46d34126583064265d4ee7109e7c7
Heikki Partanen <heikki.partanen@f-secure.com>
2016-11-28 23:46:40 +0000

    Log each incoming request

--- phase-0/server.py
+++ phase-0/server.py
@@ -59,6 +59,7 @@ def process_message(msg):
     # Try to get the parts of the message from the MESSAGES dictionary.
     # If it's not there, create one that has None in both parts
     msg = MESSAGES.get(msg_id, dict(sent=False, parts=[None] * total_parts))
+    print "received", msg_id, part_number, data
 
     # store this part of the message in the correct part of the list
     msg['parts'][part_number] = data
@@ -72,7 +73,7 @@ def process_message(msg):
             print "not sending again"
             return 'OK'
         # app.logger.debug("got a complete message for %s" % msg_id)
-        print "have all parts"
+        print "have all parts, sending response to", API_BASE
         # We can build the final message.
         result = ''.join(msg['parts'])
         # sending the response to the score calculator
@@ -83,13 +84,10 @@ def process_message(msg):
         APP.logger.debug("ID: %s" % msg_id)
         APP.logger.debug("RESULT: %s" % result)
         url = API_BASE + '/' + msg_id
-        print url
-        print result
         req = urllib2.Request(url, data=result, headers={'x-gameday-token':ARGS.API_token})
         resp = urllib2.urlopen(req)
         resp.close()
         msg['sent'] = True
-        print resp
 
     return 'OK'
 
